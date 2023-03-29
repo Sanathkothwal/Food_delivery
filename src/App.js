9c945f3c621adf2b3632de1915deb6e3b51d01b0
sanathkothwal <sanath14082000@gmail.com>
2023-03-29 17:20:44 +0530

    food delivery initital

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Header } from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-red-700">
+      <Header />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary bg-amber-400">hi</div>
+  )
+}
+
+export default Header
